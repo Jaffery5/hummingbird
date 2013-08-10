efcf4e0cc06ffef7c24e5fd53a7382fdf5c8df3a
jaffery <iftikhar0005@hotmail.com>
2013-08-10 15:34:35 -0400

    S3 Configured

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -11,4 +11,17 @@ end
 # Disable root element in JSON by default.
 ActiveSupport.on_load(:active_record) do
   self.include_root_in_json = false
+
+
+  Spree.config do |config|
+  config.use_s3 = true
+  config.s3_bucket = '<bucket>'
+  config.s3_access_key = "<key>"
+  config.s3_secret = "<secret>"
+end
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
+
 end
