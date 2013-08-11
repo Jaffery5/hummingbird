c99953929ff94c142b878b715f77e6d8f763c4fa
jaffery <iftikhar0005@hotmail.com>
2013-08-10 23:01:32 -0400

    Reinitialized repo

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -20,8 +20,8 @@ ActiveSupport.on_load(:active_record) do
   config.s3_secret = "<secret>"
 end
 
-#Paperclip.interpolates(:s3_eu_url) do |attachment, style|
-#{}"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-#end
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
 
 end
